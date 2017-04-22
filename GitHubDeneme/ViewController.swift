44ca55068982b5c9c1725226fa8d9a11a517936a
Burak GÖKÇINAR <burakgokcinar@gmail.com>
2017-04-22 09:57:33 +0300

    Background color…

---
+++ GitHubDeneme/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  GitHubDeneme
+//
+//  Created by Burak GÖKÇINAR on 22.04.2017.
+//  Copyright © 2017 SkyPlane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.blue
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
